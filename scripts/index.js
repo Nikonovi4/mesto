f61cdc789655dc3d33be566ae4499b8e650c4ccf
Nikonovi4 <nikonovi4v@yandex.ru>
2023-02-19 17:22:01 +0300

    шестой проект после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
-const editButton = document.querySelector('.profile__edit-button');
+const buttonEdit = document.querySelector('.profile__edit-button');
 const popupEditProfile = document.querySelector('.popup_profile');
 
 const formEditProfile = popupEditProfile.querySelector('.popup__form');
 
-const nameInput = formEditProfile.querySelector('.popup__input_content_name');
-const activityInput = formEditProfile.querySelector('.popup__input_content_activity');
+const nameInput = formEditProfile.querySelector('.popup__input-name');
+const activityInput = formEditProfile.querySelector('.popup__input-activity');
 
 const profileName = document.querySelector('.profile__name');
 const pofileActivity = document.querySelector('.profile__activity');
@@ -13,10 +13,9 @@ const photoContainer = document.querySelector('.foto');
 const popupAddPhoto = document.querySelector('.popup_add-photo');
 const popupAddContentButton = document.querySelector('.profile__add-content-button');
 
-const newCardName = document.querySelector('.popup__input_card_name');
-const newCardLink = document.querySelector('.popup__input_card_link');
+const newCardName = document.querySelector('.popup__input-card-name');
+const newCardLink = document.querySelector('.popup__input-card-link');
 const cardTemplate = document.querySelector('#card-template').content;
-
 
 const popupBigSizePhoto = document.querySelector('.popup_bigphoto');
 const bigSizePhoto = popupBigSizePhoto.querySelector('.popup__photo');
@@ -24,14 +23,24 @@ const bigSizePhotoName = popupBigSizePhoto.querySelector('.popup__photo-name');
 
 const formNewPhoto = document.querySelector('.popup__newphoto');
 
-function openPopup(element) {
+const buttonSubmitAddContent = popupAddPhoto.querySelector(validationConfig.submitButtonClass);
+
+const openPopup = (element) =>{
     element.classList.add('popup_opened');
+    document.addEventListener('keydown', closePopupByEsc)
+}
+
+const closePopupByEsc = (evt) => {
+    if (evt.key === 'Escape') {
+        const openedPopup = document.querySelector('.popup_opened');
+        closePopup(openedPopup);
+    }
 };
 
-function closePopup(element) {
+const closePopup = (element) => {
     element.classList.remove('popup_opened');
-};
-
+    document.removeEventListener('keydown', closePopupByEsc)
+}
 
 function autofillPopup() {
     nameInput.value = profileName.textContent;
@@ -39,23 +48,12 @@ function autofillPopup() {
     openPopup(popupEditProfile);
 };
 
-editButton.addEventListener('click', autofillPopup);
-
-document.querySelectorAll('.popup__close-button').forEach(button => {
-    const buttonsPopup = button.closest('.popup');
-    button.addEventListener('click', () => {
-        closePopup(buttonsPopup);
-    });
-});
-
-function editProfileFormSubmit(evt) {
+function heandelProfileFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     pofileActivity.textContent = activityInput.value;
     closePopup(popupEditProfile);
 };
-
-formEditProfile.addEventListener('submit', editProfileFormSubmit);
 
 function createCard(nameCard, linkCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
@@ -65,8 +63,7 @@ function createCard(nameCard, linkCard) {
     cardElement.querySelector('.card__name').textContent = nameCard;
     photo.src = linkCard;
     photo.alt = nameCard;
-
-
+    
     cardElement.querySelector('.card__del-btn').addEventListener('click', () => {
         cardElement.remove();
     });
@@ -92,15 +89,14 @@ function renderCard(nameCard, linkCard) {
 initialCards.forEach(function (item) {
     const nameCard = item.name;
     const linkCard = item.link;
-
+    
     renderCard(nameCard, linkCard);
 });
 
+
 popupAddContentButton.addEventListener('click', () => {
-    const button = popupAddPhoto.querySelector(validationConfig.submitButtonClass);
-    
     openPopup(popupAddPhoto);
-    disableButton(button, validationConfig.disableButtonClass);
+    disableButton(buttonSubmitAddContent, validationConfig.disableButtonClass);
     resetErrorElements(popupAddPhoto, validationConfig.inputSelector, validationConfig.invalidInputClass);
 });
 
@@ -113,20 +109,16 @@ function addNewCard(evt) {
     formNewPhoto.reset();
 };
 
-formNewPhoto.addEventListener('submit', addNewCard);
-
 document.querySelectorAll('.popup').forEach(popup => {
     popup.addEventListener('click', (evt) => {
-        if (evt.target === popup) {
-        closePopup(popup);
+        if (evt.target === popup || evt.target.classList.contains('popup__close-button')) {
+            closePopup(popup);
         }
     })
 });
 
-document.querySelectorAll('.popup').forEach(popup => {
-    document.addEventListener('keydown', (evt) => {
-        if (evt.key === 'Escape' && popup.classList.contains('popup_opened')) {
-        closePopup(popup);
-        }
-    })
-});
+buttonEdit.addEventListener('click', autofillPopup);
+
+formEditProfile.addEventListener('submit', heandelProfileFormSubmit);
+
+formNewPhoto.addEventListener('submit', addNewCard);
