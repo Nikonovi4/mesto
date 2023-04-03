b525b5b597e69f1a69b42514f39762aea5fa51d4
Nikonovi4 <nikonovi4v@yandex.ru>
2023-04-03 16:31:02 +0300

    Updates

--- index.js
+++ index.js
@@ -24120,7 +24120,7 @@ class Api {
     this.url = config.url;
   }
   getAllCards() {
-    fetch(this.url, {
+    return fetch(this.url, {
       method: "GET",
       headers: {
         authorization: '23f5b49e-3722-4d4b-b616-4f4f71d989aa',
@@ -24729,7 +24729,7 @@ const api = new _components_Api_js__WEBPACK_IMPORTED_MODULE_10__["default"]({
   url: 'https://mesto.nomoreparties.co/v1/cohort-63/cards'
 });
 api.getAllCards().then(list => {
-  return console.log(list);
+  console.log(list);
 });
 
 //   const photos = new Section(
