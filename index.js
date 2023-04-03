4c564a81cb1848dda32386be6c688842f5b17201
Nikonovi4 <nikonovi4v@yandex.ru>
2023-04-03 07:17:16 +0300

    Updates

--- index.js
+++ index.js
@@ -24730,8 +24730,7 @@ const api = new _components_Api_js__WEBPACK_IMPORTED_MODULE_10__["default"]({
     authorization: '23f5b49e-3722-4d4b-b616-4f4f71d989aa'
   }
 });
-const photoListJson = api.getAllCards();
-photoListJson.then(list => {
+api.getAllCards().then(list => {
   console.log(list);
 });
 
