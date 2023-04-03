a15566fa0a3b18779289abf6ec81b587f854764c
Nikonovi4 <nikonovi4v@yandex.ru>
2023-04-03 11:09:06 +0300

    Updates

--- index.js
+++ index.js
@@ -24128,8 +24128,6 @@ class Api {
       }
     }).then(res => {
       return res.json();
-    }).then(list => {
-      console.log(list);
     });
   }
 }
@@ -24730,7 +24728,9 @@ addNewPhotoForm.setEventListeners();*/
 const api = new _components_Api_js__WEBPACK_IMPORTED_MODULE_10__["default"]({
   url: 'https://mesto.nomoreparties.co/v1/cohort-63/cards'
 });
-api.getAllCards();
+api.getAllCards().then(list => {
+  console.log(list);
+});
 //   .then((list) => {
 //   const photos = new Section(
 //     {
